7cfab89b1adc893a8bd9567722e159a24527341e
Erin Blankenship <emorgan9119@gmail.com>
2026-08-07 18:06:21 -0500

    Project 1 (Bagels) start
    - Added `1. Bagels` dir
    - Created `bagels. py`
    - Added comments, notes, and early structure for project
    - Wrote initial prompt display

---
+++ 1. Bagels/bagels.py
@@ -0,0 +1,24 @@
+"""
+Bagels, a deductive logic game.
+Designed by Al Sweigart; this instance built by Erin
+"""
+
+# Research import options for this.
+
+def main():
+    print('''I am thinking of a 3-digit number. Try to guess what it is.
+    Here are some clues:
+    When I say:     That means:
+      Pico          One digit is correct but in the wrong position.
+      Fermi         One digit is correct and in the right position.
+      Bagels        No digit is correct.''')
+    
+    """
+    To Do:
+    - Create instance of random number
+    - Prompt user for first guess
+    - Response loop
+    - Build larger loop (for i in range(0,10))
+    - Do you want to play again? y/n
+        - if no, "Thanks for playing!"
+    """
